b011bc42be17a5c168301ac7215f6b2081517389
前田貴範 <maeda@goga.co.jp>
2019-02-19 22:03:13 +0900

    react-helmet導入&対応

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import {Helmet} from "react-helmet";
 import { renderPlaygroundPage } from 'graphql-playground-html';
 import { Layout } from './Layout';
 import { Content } from './Content';
 
 export default (): JSX.Element => {
   return (
-    <Layout>
-      <Content />
-    </Layout>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Hello Gatsby</title>
+        <script>
+          alert('Hello Gatsby');
+        </script>>
+      </Helmet>
+      <Layout>
+        <Content />
+      </Layout>
+    </>
   );
 };
